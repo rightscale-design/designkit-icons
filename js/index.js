46bf6f884527a06fbaf8536a565f978e41f8ce1f
jasonmelgoza <jason@rightscale.com>
2015-12-09 23:34:49 -0800

    Updated icons

--- js/index.js
+++ js/index.js
@@ -38,46 +38,10 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-api.svg'
     }
   }, {
-    id: 3,
-    item: {
-      name: 'Arrow Down',
-      svg: 'svg/icon-arrow-down.svg'
-    }
-  }, {
-    id: 4,
-    item: {
-      name: 'Arrow Right',
-      svg: 'svg/icon-arrow-right.svg'
-    }
-  }, {
-    id: 5,
-    item: {
-      name: 'Arrow Left',
-      svg: 'svg/icon-arrow-left.svg'
-    }
-  }, {
-    id: 6,
-    item: {
-      name: 'Arrow Up',
-      svg: 'svg/icon-arrow-up.svg'
-    }
-  }, {
-    id: 7,
-    item: {
-      name: 'Arrow',
-      svg: 'svg/icon-arrow.svg'
-    }
-  }, {
     id: 8,
     item: {
       name: 'Attachment',
       svg: 'svg/icon-attachment.svg'
-    }
-  }, {
-    id: 9,
-    item: {
-      name: 'Back Button',
-      svg: 'svg/icon-back-button.svg'
     }
   }, {
     id: 10,
@@ -110,12 +74,6 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-card-view.svg'
     }
   }, {
-    id: 15,
-    item: {
-      name: 'Caret',
-      svg: 'svg/icon-caret-2.svg'
-    }
-  }, {
     id: 16,
     item: {
       name: 'Caret Circle',
@@ -128,22 +86,10 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-caret.svg'
     }
   }, {
-    id: 18,
-    item: {
-      name: 'Check',
-      svg: 'svg/icon-check.svg'
-    }
-  }, {
     id: 19,
     item: {
       name: 'Checkmark Circled',
       svg: 'svg/icon-checkmark-circled.svg'
-    }
-  }, {
-    id: 20,
-    item: {
-      name: 'Close Blue',
-      svg: 'svg/icon-close-blue.svg'
     }
   }, {
     id: 21,
@@ -152,58 +98,16 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-close.svg'
     }
   }, {
-    id: 22,
-    item: {
-      name: 'Cloud',
-      svg: 'svg/icon-cloud.svg'
-    }
-  }, {
-    id: 23,
-    item: {
-      name: 'Cloud 2',
-      svg: 'svg/icon-cloud-2.svg'
-    }
-  }, {
     id: 24,
     item: {
       name: 'Cog',
       svg: 'svg/icon-cog.svg'
     }
   }, {
-    id: 25,
-    item: {
-      name: 'Colon',
-      svg: 'svg/icon-colon.svg'
-    }
-  }, {
-    id: 26,
-    item: {
-      name: 'Copy',
-      svg: 'svg/icon-copy.svg'
-    }
-  }, {
     id: 27,
     item: {
       name: 'Cloud',
       svg: 'svg/icon-cloud-2.svg'
-    }
-  }, {
-    id: 28,
-    item: {
-      name: 'Delete',
-      svg: 'svg/icon-delete.svg'
-    }
-  }, {
-    id: 29,
-    item: {
-      name: 'Docs',
-      svg: 'svg/icon-docs.svg'
-    }
-  }, {
-    id: 30,
-    item: {
-      name: 'Download',
-      svg: 'svg/icon-download.svg'
     }
   }, {
     id: 31,
@@ -218,52 +122,16 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-email.svg'
     }
   }, {
-    id: 33,
-    item: {
-      name: 'Error',
-      svg: 'svg/icon-error.svg'
-    }
-  }, {
     id: 34,
     item: {
       name: 'Expand',
       svg: 'svg/icon-expand.svg'
     }
   }, {
-    id: 35,
-    item: {
-      name: 'Export',
-      svg: 'svg/icon-export.svg'
-    }
-  }, {
-    id: 36,
-    item: {
-      name: 'External Blue',
-      svg: 'svg/icon-external-blue.svg'
-    }
-  }, {
-    id: 37,
-    item: {
-      name: 'External Grey',
-      svg: 'svg/icon-external-grey.svg'
-    }
-  }, {
     id: 38,
     item: {
       name: 'External',
       svg: 'svg/icon-external.svg'
-    }
-  }, {
-    id: 39,
-    item: {
-      name: 'Feedback',
-      svg: 'svg/icon-feedback.svg'
-    }
-  }, {
-    id: 40,
-    item: {
-      name: 'Filter',
-      svg: 'svg/icon-filter.svg'
     }
   }, {
     id: 41,
@@ -278,46 +146,10 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-folder.svg'
     }
   }, {
-    id: 42,
-    item: {
-      name: 'Gear',
-      svg: 'svg/icon-gear.svg'
-    }
-  }, {
-    id: 43,
-    item: {
-      name: 'Hide',
-      svg: 'svg/icon-hide.svg'
-    }
-  }, {
-    id: 44,
-    item: {
-      name: 'Home',
-      svg: 'svg/icon-home.svg'
-    }
-  }, {
-    id: 45,
-    item: {
-      name: 'Info Help',
-      svg: 'svg/icon-info-help.svg'
-    }
-  }, {
-    id: 45,
-    item: {
-      name: 'Info',
-      svg: 'svg/icon-info.svg'
-    }
-  }, {
     id: 46,
     item: {
       name: 'Key',
       svg: 'svg/icon-key.svg'
-    }
-  }, {
-    id: 47,
-    item: {
-      name: 'Link',
-      svg: 'svg/icon-link.svg'
     }
   }, {
     id: 48,
@@ -326,106 +158,16 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
       svg: 'svg/icon-list-view.svg'
     }
   }, {
-    id: 49,
-    item: {
-      name: 'Lock',
-      svg: 'svg/icon-lock.svg'
-    }
-  }, {
-    id: 50,
-    item: {
-      name: 'Logout',
-      svg: 'svg/icon-logout.svg'
-    }
-  }, {
-    id: 51,
-    item: {
-      name: 'Markdown',
-      svg: 'svg/icon-markdown.svg'
-    }
-  }, {
     id: 52,
     item: {
       name: 'Phone',
       svg: 'svg/icon-phone.svg'
     }
   }, {
-    id: 53,
-    item: {
-      name: 'Plus Open',
-      svg: 'svg/icon-plus-open.svg'
-    }
-  }, {
-    id: 54,
-    item: {
-      name: 'Product Trigger',
-      svg: 'svg/icon-product-trigger.svg'
-    }
-  }, {
-    id: 55,
-    item: {
-      name: 'Read Only',
-      svg: 'svg/icon-read-only.svg'
-    }
-  }, {
-    id: 56,
-    item: {
-      name: 'Refresh',
-      svg: 'svg/icon-refresh.svg'
-    }
-  }, {
-    id: 57,
-    item: {
-      name: 'Save',
-      svg: 'svg/icon-save.svg'
-    }
-  }, {
-    id: 58,
-    item: {
-      name: 'Search',
-      svg: 'svg/icon-search-blue.svg'
-    }
-  }, {
-    id: 59,
-    item: {
-      name: 'Secure',
-      svg: 'svg/icon-secure.svg'
-    }
-  }, {
-    id: 60,
-    item: {
-      name: 'Share',
-      svg: 'svg/icon-share.svg'
-    }
-  }, {
     id: 61,
     item: {
       name: 'Shield',
       svg: 'svg/icon-shield.svg'
-    }
-  }, {
-    id: 62,
-    item: {
-      name: 'Sort Down',
-      svg: 'svg/icon-sort-down.svg'
-    }
-  }, {
-    id: 63,
-    item: {
-      name: 'Sort Up',
-      svg: 'svg/icon-sort-up.svg'
-    }
-  }, {
-    id: 64,
-    item: {
-      name: 'Sort Unsorted',
-      svg: 'svg/icon-sort-unsorted.svg'
-    }
-  }, {
-    id: 65,
-    item: {
-      name: 'Sort Ticket',
-      svg: 'svg/icon-ticket.svg'
     }
   }, {
     id: 65,
