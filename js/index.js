d83e69ca552e51b399e3df70d9e5d00dcd441e78
jasonmelgoza <jason@rightscale.com>
2016-02-11 13:19:16 -0800

    Updated icon format

--- js/index.js
+++ js/index.js
@@ -28,230 +28,236 @@ myApp.controller('iconsCtrl', function($scope, $routeParams, $location) {
     id: 1,
     item: {
       name: 'Cloud',
-      svg: 'svg/icon-cloud.svg'
+      svg: 'svg/icon-cloud.svg#icon-cloud'
     }
   }, {
     id: 2,
     item: {
       name: 'Book',
-      svg: 'svg/icon-book.svg'
+      svg: 'svg/icon-book.svg#icon-book'
     }
   }, {
     id: 3,
     item: {
       name: 'Brush',
-      svg: 'svg/icon-brush.svg'
+      svg: 'svg/icon-brush.svg#icon-brush'
     }
   }, {
     id: 4,
     item: {
       name: 'Admin',
       class: '.icon-admin',
-      svg: 'svg/icon-admin.svg'
+      svg: 'svg/icon-admin.svg#icon-admin'
     }
   }, {
     id: 5,
     item: {
       name: 'Beta',
-      svg: 'svg/icon-beta.svg'
+      svg: 'svg/icon-beta.svg#icon-beta'
     }
   }, {
     id: 6,
     item: {
       name: 'Attachment',
-      svg: 'svg/icon-attachment.svg'
+      svg: 'svg/icon-attachment.svg#icon-attachment'
     }
   }, {
     id: 7,
     item: {
       name: 'API',
-      svg: 'svg/icon-api.svg'
+      svg: 'svg/icon-api.svg#icon-api'
     }
   }, {
     id: 8,
     item: {
       name: 'Link',
-      svg: 'svg/icon-link.svg'
+      svg: 'svg/icon-link.svg#icon-link'
     }
   }, {
     id: 9,
     item: {
       name: 'Calendar',
-      svg: 'svg/icon-calendar.svg'
+      svg: 'svg/icon-calendar.svg#icon-calendar'
     }
   }, {
     id: 10,
     item: {
       name: 'Check',
-      svg: 'svg/icon-check.svg'
+      svg: 'svg/icon-check.svg#icon-check'
     }
   }, {
     id: 11,
     item: {
       name: 'Card View',
-      svg: 'svg/icon-card-view.svg'
+      svg: 'svg/icon-card-view.svg#icon-view-card'
     }
   }, {
     id: 12,
     item: {
       name: 'List View',
-      svg: 'svg/icon-list-view.svg'
+      svg: 'svg/icon-list-view.svg#icon-view-list'
     }
   }, {
     id: 13,
     item: {
       name: 'Caret Circle',
-      svg: 'svg/icon-caret-circle.svg'
+      svg: 'svg/icon-caret-circle.svg#icon-caret-circle'
     }
   }, {
     id: 14,
     item: {
       name: 'Checkmark Circled',
-      svg: 'svg/icon-checkmark-circled.svg'
+      svg: 'svg/icon-checkmark-circled.svg#icon-check-circle'
     }
   }, {
     id: 15,
     item: {
       name: 'Close',
-      svg: 'svg/icon-close.svg'
+      svg: 'svg/icon-close.svg#icon-close'
     }
   }, {
     id: 16,
     item: {
       name: 'Cog',
-      svg: 'svg/icon-cog.svg'
+      svg: 'svg/icon-cog.svg#icon-cog'
     }
   }, {
     id: 17,
     item: {
       name: 'Edit',
-      svg: 'svg/icon-edit.svg'
+      svg: 'svg/icon-edit.svg#icon-edit'
     }
   }, {
     id: 18,
     item: {
       name: 'Email',
-      svg: 'svg/icon-email.svg'
+      svg: 'svg/icon-email.svg#icon-email'
     }
   }, {
     id: 19,
     item: {
       name: 'Expand',
-      svg: 'svg/icon-expand.svg'
+      svg: 'svg/icon-expand.svg#icon-expand'
     }
   }, {
     id: 20,
     item: {
       name: 'External',
-      svg: 'svg/icon-external.svg'
+      svg: 'svg/icon-external.svg#icon-external'
     }
   }, {
     id: 21,
     item: {
       name: 'Folded Map',
-      svg: 'svg/icon-folded-map.svg'
+      svg: 'svg/icon-folded-map.svg#icon-folded-map'
     }
   }, {
     id: 22,
     item: {
       name: 'Folder',
-      svg: 'svg/icon-folder.svg'
+      svg: 'svg/icon-folder.svg#icon-folder'
     }
   }, {
     id: 23,
     item: {
       name: 'Key',
-      svg: 'svg/icon-key.svg'
+      svg: 'svg/icon-key.svg#icon-key'
     }
   }, {
     id: 24,
     item: {
       name: 'Phone',
-      svg: 'svg/icon-phone.svg'
+      svg: 'svg/icon-phone.svg#icon-phone'
     }
   }, {
     id: 25,
     item: {
       name: 'Shield',
-      svg: 'svg/icon-shield.svg'
+      svg: 'svg/icon-shield.svg#icon-shield'
     }
   }, {
     id: 26,
     item: {
       name: 'Trash',
-      svg: 'svg/icon-trash.svg'
+      svg: 'svg/icon-trash.svg#icon-trash'
     }
   }, {
     id: 27,
     item: {
       name: 'User',
-      svg: 'svg/icon-user.svg'
+      svg: 'svg/icon-user.svg#icon-user'
     }
   }, {
     id: 28,
     item: {
       name: 'Disable',
-      svg: 'svg/icon-disable.svg'
+      svg: 'svg/icon-disable.svg#icon-disable'
     }
   }, {
     id: 29,
     item: {
       name: 'Lock',
-      svg: 'svg/icon-lock.svg'
+      svg: 'svg/icon-lock.svg#icon-lock'
     }
   }, {
     id: 30,
     item: {
       name: 'Search',
-      svg: 'svg/icon-search.svg'
+      svg: 'svg/icon-search.svg#icon-search'
     }
   }, {
     id: 31,
     item: {
       name: 'Caret',
-      svg: 'svg/icon-caret.svg'
+      svg: 'svg/icon-caret.svg#icon-caret'
     }
   }, {
     id: 32,
     item: {
-      name: 'More',
-      svg: 'svg/icon-more-vertical.svg'
+      name: 'More Vertical',
+      svg: 'svg/icon-more-vertical.svg#icon-more-vertical'
     }
   }, {
     id: 33,
     item: {
-      name: 'More',
-      svg: 'svg/icon-more-horizontal.svg'
+      name: 'More Horizontal',
+      svg: 'svg/icon-more-horizontal.svg#icon-more-horizontal'
     }
   }, {
     id: 34,
     item: {
       name: 'Code Download',
-      svg: 'svg/icon-code-download.svg'
+      svg: 'svg/icon-code-download.svg#icon-code-download'
     }
   }, {
     id: 35,
     item: {
       name: 'Tooltip',
-      svg: 'svg/icon-tooltip.svg'
+      svg: 'svg/icon-tooltip.svg#icon-tooltip'
     }
   }, {
     id: 36,
     item: {
       name: 'Alert',
-      svg: 'svg/icon-alert.svg'
+      svg: 'svg/icon-alert.svg#icon-alert'
     }
   }, {
     id: 37,
     item: {
       name: 'Bell',
-      svg: 'svg/icon-bell.svg'
+      svg: 'svg/icon-bell.svg#icon-bell'
     }
   }, {
     id: 38,
     item: {
       name: 'Bell Off',
-      svg: 'svg/icon-bell-off.svg'
+      svg: 'svg/icon-bell-off.svg#icon-bell-off'
+    }
+  }, {
+    id: 39,
+    item: {
+      name: 'Stop',
+      svg: 'svg/icon-stop.svg#icon-stop'
     }
   }];
 
